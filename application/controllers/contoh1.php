9ad641a0c9aa0f52d5b383ad4ec0c7bdd6cf32c4
wawanzs8 <wawanwawanck@gmail.com>
2024-03-25 10:06:56 +0700

    MENAMBAH FILE BIODATA

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index()
+  {
+    echo "<h1>Perkenalkan<h1>";
+    echo "Nama Saya Wawan Kurniawan
+    Saya Tinggal Di Kota Pontianak Olahraga Yang saya sukai futsal & badminton";
+  }
+
+}
