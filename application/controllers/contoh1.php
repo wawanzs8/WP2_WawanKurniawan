948667d059538c94ad68259dc976bafa18fbbe91
wawanzs8 <wawanwawanck@gmail.com>
2024-03-25 10:37:30 +0700

    tugas data diri

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -5,7 +5,7 @@ class Contoh1 extends CI_Controller
   {
     echo "<h1>Perkenalkan<h1>";
     echo "Nama Saya Wawan Kurniawan
-    Saya Tinggal Di Kota Pontianak Olahraga Yang saya sukai futsal & badminton";
+    Saya Tinggal Di Kota Pontianak Olahraga Yang saya sukai futsal & badminton";  
   }
 
 }
