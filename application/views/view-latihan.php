76b31fa3d7c2d1c7b25a34099a5af8520a9090d5
wawanzs8 <wawanwawanck@gmail.com>
2024-03-30 14:57:38 +0700

    menambahkan file tugas baru contoh1,2,3 pada pertemuan 3

---
+++ application/views/view-latihan.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Latihan</title>
+</head>
+<body>
+  Halo Kawan.. Yuk kita belajar web programing..!!!<br>
+  Nilai 1 = <?= $nilai1; ?>
+  Nilai 2 = <?= $nilai2; ?>
+  ini hasil dari pemodelan dengan metode penjumlahan yaitu <?=$nilai1 . " + " .$nilai2. " = ".$hasil; ?> 
+</body>
+
+</html>
