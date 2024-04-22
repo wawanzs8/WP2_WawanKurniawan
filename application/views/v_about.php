cbb2441ebd485970d52a9239909daf59fa38f8f1
wawanzs8 <wawanwawanck@gmail.com>
2024-04-22 19:32:50 +0700

    Pertemuan 4 dan Tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+  <li>Nama Depan : Wawan</li>
+  <li>Nama Belakang : Kurniawan</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+  <ul type="none">
+    <li> Jungkat</li>
+  </ul>
+ 
+ <h4>Tempat Lahir</h4>
+  <ul type="none">
+    <li>wajok hilir</li>
+  </ul>
+ <h4>Olah Raga Favorit</h4>
+  <ul type="square">
+    <li>Bulutangkis</li>
+    <li>futsalr</li>
+  </ul>
+</section>
