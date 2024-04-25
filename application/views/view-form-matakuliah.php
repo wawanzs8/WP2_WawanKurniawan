29b947ab792904cb08ecd5e1b0b8109b3a327847
wawanzs8 <wawanwawanck@gmail.com>
2024-04-25 16:54:50 +0700

    Pertemuan 5 dan tugas 4

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -25,7 +25,8 @@
         <th>Kode MTK</th>
         <th>:</th>
         <td>
-          <input type="text" name="kode" id="kode">
+          <input type="text" name="kode" id="kode" placeholder="isikan data">
+          <div class="invalid-feedback"><?= form_error('kode') ?></div>
         </td>
       </tr>
 
@@ -33,7 +34,8 @@
         <th>Nama MTK</th>
         <td>:</td>
         <td>
-          <input type="text" name="nama" id="kode">
+          <input type="text" name="nama" id="nama" placeholder="isikan data">
+          <div class="invalid-feedback"><?= form_error('nama') ?></div>
         </td>
       </tr>
 
@@ -48,6 +50,7 @@
             <option value="4">4</option>
 
           </select>
+          <div class="invalid-feedback"><?= form_error('sks')?></div>
         </td>
       </tr>
 
